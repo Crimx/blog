ab81c684b78aae0e7f75b9ac3716c43dc10ce73f
CRIMX <straybugs@gmail.com>
2016-11-21 01:41:48 +0800

    updated cover position helper

--- themes/crimx/scripts/read_cover_focus.js
+++ themes/crimx/scripts/read_cover_focus.js
@@ -1,3 +1,8 @@
+/* global hexo */
 const fs = require('fs')
 
-hexo.config.cover_position = JSON.parse(fs.readFileSync('./source/images/cover/_focus.json').toString())
+const data = JSON.parse(fs.readFileSync('./source/images/cover/_focus.json').toString())
+
+hexo.extend.helper.register('get_cover_position', function (image) {
+  return data[image] || 'center'
+})
