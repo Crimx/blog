9de1f45dc3b7716b55a02678b1c127d036dacc9f
CRIMX <straybugs@gmail.com>
2016-11-26 17:53:05 +0800

    added client-side generated recent posts

--- themes/crimx/source/_js/main.js
+++ themes/crimx/source/_js/main.js
@@ -17,6 +17,7 @@ var requestAnimationFrame =
 $(document).ready(function () {
   siteMenuHandler()
   siteTocHandler()
+  recentPosts()
 })
 
 function siteMenuHandler () {
@@ -175,5 +176,46 @@ function siteTocHandler () {
         return false
       }
     })
+  })
+}
+
+function recentPosts () {
+  var $newArticles = $('.new-articles')
+  if ($newArticles.length <= 0) { return }
+
+  $.getJSON('/recent-posts.json', function (posts) {
+    var articles = $.map(posts, function (post, i) {
+      if (i > 4) { return false }
+      //  <li class="new-articles__item">
+      //    <a class="new-articles__link" href="[url]">
+      //      <div class="new-articles__cover" style="background:no-repeat url([cover]) [position]/cover">
+      //      </div>
+      //      <h1 class="new-articles__title">[title]</h1>
+      //    </a>
+      //  </li>
+      var $title = $('<h1>', {
+        class: 'new-articles__title',
+        text: post.title
+      })
+
+      var $cover = $('<div>', {
+        class: 'new-articles__cover',
+        css: {
+          background: 'no-repeat url(' + post.cover + ') ' + post.position + '/cover'
+        }
+      })
+
+      var $link = $('<a>', {
+        class: 'new-articles__link',
+        href: post.url
+      })
+      .append([$cover, $title])
+
+      return $('<li>', {
+        class: 'new-articles__item'
+      }).append($link)
+    })
+
+    $newArticles.empty().append(articles)
   })
 }
