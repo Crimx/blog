6ad533093d14d181a09ec223249a06896a408cbd
CRIMX <straybugs@gmail.com>
2016-11-26 17:49:46 +0800

    updated _focus.json -> _cover-position.json

--- themes/crimx/scripts/hexo-helper-cover-position.js
+++ themes/crimx/scripts/hexo-helper-cover-position.js
@@ -1,7 +1,5 @@
 /* global hexo */
-const fs = require('fs')
-
-const data = JSON.parse(fs.readFileSync('./source/images/cover/_focus.json').toString())
+const data = require('../../../source/images/cover/_cover-positions.json')
 
 hexo.extend.helper.register('get_cover_position', function (image) {
   return data[image] || 'center'
