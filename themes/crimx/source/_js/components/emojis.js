c02fb0863530a3fa595ecb78311731dda6412b12
CRIMX <straybugs@gmail.com>
2016-12-25 14:56:58 +0800

    updated emojis.js for no reason

--- themes/crimx/source/_js/components/emojis.js
+++ themes/crimx/source/_js/components/emojis.js
@@ -6,11 +6,11 @@ $('span.github-emoji').each(function (i, emoji) {
   var $emoji = $(emoji)
   var codepoint = $emoji.html()
   $('<img height="20" width="20">')
-    .prop('src', $emoji.data('src'))
-    .prop('alt', $emoji.attr('title'))
     .on('error', function () {
       // image loading failed
       $emoji.html(codepoint)
     })
+    .prop('alt', $emoji.attr('title'))
+    .prop('src', $emoji.data('src'))
     .appendTo($emoji.empty())
 })
