75d5af7307c0e42c4e6df18f077b17463dcf81fc
CRIMX <straybugs@gmail.com>
2016-12-27 02:37:04 +0800

    updated progressive recommended posts

--- themes/crimx/source/_js/components/footer.js
+++ themes/crimx/source/_js/components/footer.js
@@ -1,45 +1,88 @@
 'use strict'
 /* global $ */
 
+var progressiveBg = require('../tools/progressive-background-image')
+
 /* ------------------------------------ *\
-     Generates 4 recent posts
+     Get 4 random recommended posts
 \* ------------------------------------ */
-var $newArticles = $('.new-articles')
-if ($newArticles.length > 0) {
-  $.getJSON('/recent-posts.json', function (posts) {
-    var articles = $.map(posts, function (post, i) {
-      if (i >= 4) { return }
-      //  <li class="new-articles__item">
-      //    <a class="new-articles__link" href="[url]">
-      //      <div class="new-articles__cover" style="background:no-repeat url([cover]) [position]/cover">
-      //      </div>
-      //      <h1 class="new-articles__title">[title]</h1>
-      //    </a>
-      //  </li>
-      var $title = $('<h1>', {
-        class: 'new-articles__title',
-        text: post.title
-      })
+var $recommendedPost = $('.recommended-post')
+if ($recommendedPost.length > 0) {
+  var $window = $(window)
+  var siteFooterTop = $('.site-footer').offset().top
 
-      var $cover = $('<div>', {
-        class: 'new-articles__cover',
-        css: {
-          background: 'no-repeat url(' + post.cover + ') ' + post.position + '/cover'
-        }
-      })
+  var scrollHandler = function scrollHandler () {
+    var scrollBottom = $window.scrollTop() + $window.height()
 
-      var $link = $('<a>', {
-        class: 'new-articles__link',
-        href: post.url
-      })
-      .append([$cover, $title])
+    if (scrollBottom >= siteFooterTop) {
+      updateDom()
+      window.removeEventListener('scroll', scrollHandler, window.passiveEvents)
+    }
+  }
+  window.addEventListener('scroll', scrollHandler, window.passiveEvents)
+}
 
-      return $('<li>', {
-        class: 'new-articles__item'
-      }).append($link)
-    })
-
-    $newArticles.empty().append(articles)
-    $newArticles.addClass('new-articles--added')
+function updateDom () {
+  var itemList = $('.recommended-post__item').map(function (i, item) {
+    var $item = $(item)
+    return {
+      $link: $item.find('.recommended-post__link'),
+      $cover: $item.find('.recommended-post__cover'),
+      $thumb: $item.find('.recommended-post__thumbnail'),
+      $title: $item.find('.recommended-post__title')
+    }
   })
+
+  $.getJSON('/recommended-posts.json', function (posts) {
+    if (!$.isArray(posts)) { return }
+    posts = shuffle(posts)
+    var i = 0
+
+    itemList.forEach(function (item) {
+      var p = posts[i++]
+      item.$link.prop('href', p.url)
+      if (p.cover) {
+        item.$cover.css({
+          'background-image': 'url(' + p.cover + ')',
+          'background-position': p.position,
+          'background-size': 'cover'
+        })
+      } else {
+        item.$cover.css('background-image', 'url()')
+      }
+      item.$thumb.removeClass('progressive-bg-thumbnail--hide')
+      if (p.base64) {
+        item.$thumb.css({
+          'background-image': 'url(' + p.base64 + ')',
+          'background-position': p.position,
+          'background-size': 'cover'
+        })
+      } else {
+        item.$thumb.css('background-image', 'url()')
+      }
+      item.$title.html(p.title)
+    })
+    progressiveBg('.recommended-post')
+
+    $recommendedPost.addClass('recommended-post--added')
+  })
+}
+
+function shuffle (array) {
+  var currentIndex = array.length
+  var temporaryValue, randomIndex
+
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temporaryValue
+  }
+
+  return array
 }
