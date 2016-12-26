ce080eb4f08fec9722bc3f2d5572f7094592678b
CRIMX <straybugs@gmail.com>
2016-12-27 02:54:22 +0800

    removed old helper

--- themes/crimx/scripts/hexo-helper-cover-position.js
+++
@@ -1,6 +0,0 @@
-/* global hexo */
-const data = require('../../../source/images/cover/_cover-positions.json')
-
-hexo.extend.helper.register('get_cover_position', function (image) {
-  return data[image] || 'center'
-})
