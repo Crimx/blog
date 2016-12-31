61bde4cc6645ac7c5f91b5e51da33a0eb19ecdbf
CRIMX <straybugs@gmail.com>
2017-01-01 04:25:00 +0800

    updated recommended post parse faster

--- themes/crimx/source/_js/components/footer.js
+++ themes/crimx/source/_js/components/footer.js
@@ -35,11 +35,12 @@ function updateDom () {
 
   $.getJSON('/recommended-posts.json', function (posts) {
     if (!$.isArray(posts)) { return }
-    posts = shuffle(posts)
-    var i = 0
 
     itemList.forEach(function (item) {
-      var p = posts[i++]
+      var i = Math.floor(Math.random() * posts.length)
+      var p = posts[i]
+      posts.splice(i, 1)
+
       item.$link.prop('href', p.url)
       if (p.cover) {
         item.$cover.css({
@@ -66,23 +67,4 @@ function updateDom () {
 
     $recommendedPost.addClass('recommended-post--added')
   })
-}
-
-function shuffle (array) {
-  var currentIndex = array.length
-  var temporaryValue, randomIndex
-
-  // While there remain elements to shuffle...
-  while (currentIndex !== 0) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex)
-    currentIndex -= 1
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex]
-    array[currentIndex] = array[randomIndex]
-    array[randomIndex] = temporaryValue
-  }
-
-  return array
 }
