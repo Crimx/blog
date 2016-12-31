71087bd3289bdee9dbc9eb4efff8d677552f3676
CRIMX <straybugs@gmail.com>
2017-01-01 04:25:49 +0800

    fixed changed components loading timing

--- themes/crimx/source/_js/index.js
+++ themes/crimx/source/_js/index.js
@@ -13,17 +13,20 @@ require('./fallbacks/hairline-border')
 require('./fallbacks/passive-events')
 
 $(document).ready(function () {
-  require('./components/menu')
-  require('./components/emojis')
-  require('./components/toc')
-  require('./components/footer')
-  require('./components/search')
-
   /* ------------------------------------ *\
      TOOLs
   \* ------------------------------------ */
   require('./tools/iframe-defer')
   require('./tools/progressive-background-image')()
+
+  /* ------------------------------------ *\
+     COMPONENTs
+  \* ------------------------------------ */
+  require('./components/emojis')
+  require('./components/search')
+  require('./components/menu')
+  require('./components/toc')
+  require('./components/footer')
 
   /* ------------------------------------ *\
      POLYFILLs
