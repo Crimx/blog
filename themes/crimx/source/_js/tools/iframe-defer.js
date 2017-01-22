2e2ce481c50537d6da9debe0f9c95f95917632b9
CRIMX <straybugs@gmail.com>
2017-01-22 21:54:15 +0800

    updated youtube autoresizes

--- themes/crimx/source/_js/tools/iframe-defer.js
+++ themes/crimx/source/_js/tools/iframe-defer.js
@@ -10,13 +10,26 @@ function scrollHandler () {
     var scrollBottom = $window.scrollTop() + $window.height()
     $iframeDefers = $iframeDefers.filter(function () {
       var $iframe = $(this)
+
       if (scrollBottom >= $iframe.offset().top) {
         $iframe.prop('src', $iframe.data('src'))
 
         // youtube video
         if ($iframe.data('type') === 'youtube') {
-          $iframe.css({
-            height: $iframe.width() / $iframe.attr('width') * $iframe.attr('height') + 'px'
+          var width = Number($iframe.attr('width'))
+          var height = Number($iframe.attr('height'))
+          var resizeHandler = function () {
+            $iframe.css({
+              height: $iframe.width() / width * height + 'px'
+            })
+          }
+
+          resizeHandler()
+
+          var resizeTimeout
+          $window.on('resize', function () {
+            if (resizeTimeout) { clearTimeout(resizeTimeout) }
+            resizeTimeout = setTimeout(resizeHandler, 500)
           })
         }
 
